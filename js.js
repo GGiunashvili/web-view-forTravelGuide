5eae1ddb28593bbc8bf918d8bfa0e43242f8f7a6
GGiunashvili <gio1.giunashvili@gmail.com>
2020-02-18 14:23:46 +0400

    ++

---
+++ js.js
@@ -0,0 +1,39 @@
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
+
+
+
+
+
+
+// Hide the extra content initially, using JS so that if JS is disabled, no problemo:
+$('.read-more-content').addClass('hide')
+$('.read-more-show, .read-more-hide').removeClass('hide')
+
+// Set up the toggle effect:
+$('.read-more-show').on('click', function(e) {
+    $(this).next('.read-more-content').removeClass('hide');
+    $(this).addClass('hide');
+    e.preventDefault();
+});
+
+// Changes contributed by @diego-rzg
+$('.read-more-hide').on('click', function(e) {
+    var p = $(this).parent('.read-more-content');
+    p.addClass('hide');
+    p.prev('.read-more-show').removeClass('hide'); // Hide only the preceding "Read More"
+    e.preventDefault();
+});
